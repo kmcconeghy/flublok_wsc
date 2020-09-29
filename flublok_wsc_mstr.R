2c51743b42129f5fac26b62aafdc8f45dfdc7460
McConeghy <kmcconeg@ad.brown.edu>
2020-09-29 10:02:40 -0400

    prepared datafile

--- flublok_wsc_mstr.R
+++ flublok_wsc_mstr.R
@@ -4,17 +4,17 @@
 #-- Load data hierarchy  
   source(here::here('src', 'flublok_wsc_lst_dtafiles.R'))
 
+#-- dataset / munge  
+  if (T) source(here::here('src', 'flublok_wsc_bld_00.R'))
+
 #-- Run models
-  if (TRUE) { 
-    source(here::here('src', 'flublok_wsc_cpt_00.R'))
-  }
+  if (F) source(here::here('src', 'flublok_wsc_cpt_00.R'))
 
 #-- Project Reports  
-  if (report==T) source(here::here('src', 'flublok_wsc_rpt_00.R'))
+  if (T) source(here::here('src', 'flublok_wsc_rpt_00.R'))
   
 #--IF ONLY WANT ONE FILE TO RUN
   #render_one('B04', wd.CodeFiles, ReportFiles)
-
 
 cat(paste0('Project Run: ', prj.RunTime %--% Sys.time()))
 
--- src/flublok_wsc_lst_dtafiles.R
+++ src/flublok_wsc_lst_dtafiles.R
@@ -1,17 +1,6 @@
 dta.names <- list(
   f_sas_list = c(''),
-  f_raw_list = c(''),
-  f_munge_list = c(''),
-  f_cpt_list = c(''),
-  f_datasets = c('')
+  f_raw_list = c('facility_2017.xlsx'),
+  f_munge_list = c('ltcfocus.Rds'),
+  f_cpt_list = c('')
 )
-
-
-#--Filename checks  
-
-#---SAS and RDS names match
-chk_run <- function() {
-  chk_f_sas_list <- str_replace(dta.names$f_sas_list, '\\..*', '') == str_replace(dta.names$f_raw_list, '\\..*', '')
-  if (any(chk_f_sas_list==F)) stop('SAS and RDS raw filenames do not match')
-}
-chk_run()
